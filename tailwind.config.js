f166e1a634c185e929a2181fbee55d3da3652af9
hPenaC <hPenaC@users.noreply.github.com>
2022-01-07 21:18:43 +0000

    deploy: be313fabcc62096750babb02eda55022a841bac3

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  mode:'jit',
-  purge: ['./src/**/*.{html,ts}', './projects/**/*.{html,ts}'],
-  content: ["./src/app/**/*.{html,ts}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
