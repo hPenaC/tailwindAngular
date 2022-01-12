baecd740777605138a76c74fe3135ddb29ba2a2e
Henry Peña <henry.pena@mayasoft.com.co>
2022-01-12 16:59:27 -0500

    components added and header update

--- src/app/monitor/pages/home/home.component.ts
+++ src/app/monitor/pages/home/home.component.ts
@@ -1,16 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { ExampleService } from 'src/app/core/services/example.service';
+import { Component, OnInit } from "@angular/core";
+import { ExampleService } from "src/app/core/services/example.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
+  constructor(public exampleService: ExampleService) {}
 
-  constructor(public exampleService: ExampleService) { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}  
+  
 }
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,22 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { fromEvent, map, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements AfterViewInit {
 
   constructor() { }
-
+  private scrollSubscription!:Subscription;
   private isClosed:boolean = true;
   private wasClicked:boolean = false;
+  @ViewChild('scroll',{static:true}) scrollBar!:ElementRef;  
 
-  ngOnInit(): void {
+  ngAfterViewInit(){
+    if(this.scrollSubscription && !this.scrollSubscription.closed){
+      this.scrollSubscription.unsubscribe();
+    }
+    this.scrollSubscription= fromEvent<Event>(document,'scroll')
+    .pipe(
+      map(this.calcularPorcentajeScroll)      
+    )
+    .subscribe(
+      (res)=>{
+        this.scrollBar.nativeElement.style.width=`${res}%`
+      }
+    );
   }
 
   public getIsClosed(): boolean {
     return this.isClosed;    
+  }
+
+  private calcularPorcentajeScroll(event:any){
+    const {
+      scrollTop,
+      scrollHeight,
+      clientHeight
+    }=event.target.documentElement;
+    return (scrollTop / (scrollHeight - clientHeight))*100;
   }
 
   public getClicked(): boolean {
@@ -28,8 +51,6 @@ export class HeaderComponent implements OnInit {
       this.wasClicked=true;
     }
     this.isClosed=!this.isClosed;
-    console.log(this.getIsClosed()&&this.getClicked());
-    console.log(this.getIsClosed());
   }
 
 }
